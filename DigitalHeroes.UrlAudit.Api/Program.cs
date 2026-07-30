840c6943177c935028d57783c6ad5d8d7a0f58bb
Karthik Raishetty <karthikraishetty90@gmail.com>
2026-07-30 20:09:45 +0530

    Enable Swagger in production

--- DigitalHeroes.UrlAudit.Api/Program.cs
+++ DigitalHeroes.UrlAudit.Api/Program.cs
@@ -77,11 +77,10 @@ var app = builder.Build();
     app.UseSerilogRequestLogging();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-{
+    
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
     app.UseRateLimiter();
     app.UseHttpsRedirection();
