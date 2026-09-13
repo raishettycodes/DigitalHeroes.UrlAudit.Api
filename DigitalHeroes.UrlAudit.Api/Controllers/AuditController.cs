8561c5817ac67dfac667a133d0d11e929f8afe5f
Karthik Raishetty <karthikraishetty90@gmail.com>
2026-09-13 19:39:06 +0530

    Document 403 audit limit response

--- DigitalHeroes.UrlAudit.Api/Controllers/AuditController.cs
+++ DigitalHeroes.UrlAudit.Api/Controllers/AuditController.cs
@@ -41,6 +41,7 @@ namespace DigitalHeroes.UrlAudit.Api.Controllers
     StatusCodes.Status200OK)]
 [ProducesResponseType(StatusCodes.Status400BadRequest)]
 [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(StatusCodes.Status403Forbidden)]
 [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
 [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 public async Task<IActionResult> Audit(
